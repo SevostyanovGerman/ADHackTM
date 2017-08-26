9c83fd88a25b5fbbb043064637b4a442cf2812e2
Sorokinsta <sorokin.sta@gmail.com>
2017-08-26 18:33:36 +0300

    Spring Boot init

---
+++ src/main/java/com/adhack/adhack/ADHackApplication.java
@@ -0,0 +1,12 @@
+package com.adhack.adhack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ADHackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ADHackApplication.class, args);
+	}
+}
---
+++ src/test/java/com/adhack/adhack/AdhachprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.adhack.adhack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdhachprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
